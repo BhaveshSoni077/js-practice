a3aac7ebd62317e203e2a92b0b6e954723f629bc
Bhavesh <sonibhavesh620@gmail.com>
2021-08-17 16:19:09 +0530

    test

---
+++ practice.js
@@ -0,0 +1,69 @@
+  /* For EACH LOOP
+    var arr=[1,2,3,4,5,6]
+    arr.forEach(function(element){
+        console.log(element);
+    })*/
+
+
+/*Function
+
+    function sum(a,b){
+        return a+b;
+    }
+*/
+
+
+/* let variable iska scope particular block ke andr rhta h
+    let j=0;
+*/
+
+
+/*
+const variable, variable ko constant bnane ke kaam aata  h iski value chng nhi kr skte appn.
+*/
+
+
+/* While LOOP
+let j=0
+  while(j<arr.length){
+      console.log(j);
+      j++;
+  }
+  */
+
+
+//var arr=[1,"shyam",true,null]
+//console.log(arr.length)
+ 
+ 
+ 
+ 
+ /*Stack functionality
+  arr.pop;
+  arr.push;
+ */
+ 
+ 
+ /* queue Functionality
+  arr.shift();                       //it will remove the first element.
+  arr.unshift("Harry");              //it will add harry at the beginning.
+  console.log(arr.unshift("Harry"))  //it will return the length of new generated array.
+ */
+ 
+ 
+//toString()
+//arr.toString();                     //convert array into string.
+ 
+ 
+//sort
+//arr.sort();                         //it will first convert array into string than it sort the element.
+ 
+
+// String Methods in javaScript
+/*
+var str1="harry Singh";
+console.log(str1.length);
+console.log(str1.slice(1,4));
+*/
+
+
